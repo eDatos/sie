41ef734bac0fa8856b06bdf7a7f948f1d8ab94d4
Javier Villamonte <jvilper@arte-consultores.com>
2018-05-22 10:09:53 +0100

    METAMAC-2750 - Despliegue en la infraestructura de DEMOS
    >Se modifica el script de despliegue.

--- sie-portal-web/etc/deploy/deploy.sh
+++ sie-portal-web/etc/deploy/deploy.sh
@@ -2,19 +2,26 @@
 
 HOME_PATH=sie
 TRANSFER_PATH=$HOME_PATH/tmp
-DEPLOY_TARGET_PATH=/servers/metamac/tomcats/sie01/webapps
 DEMO_ENV=$HOME_PATH/env
+DEPLOY_TARGET_PATH=/servers/metamac/tomcats/metamac01/webapps
+RESTART=1
+
+if [ "$1" == "--no-restart" ]; then
+    RESTART=0
+fi
 
 scp -r etc/deploy deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH
 scp target/sie-portal-web-*.war deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH/sie.war
 ssh deploy@estadisticas.arte-consultores.com <<EOF
 
-    chmod a+x $TRANSFER_PATH/deploy/*.sh;
+    # chmod a+x $TRANSFER_PATH/deploy/*.sh;
     
-    sudo service sie01 stop
+    if [ $RESTART -eq 1 ]; then
+        sudo service metamac01 stop
+    fi
     
     ###
-    # ARTE-APPLICATION-TEMPLATE
+    # SIE
     ###
 
     # Update Process
@@ -24,13 +31,14 @@ ssh deploy@estadisticas.arte-consultores.com <<EOF
     sudo rm -rf $DEPLOY_TARGET_PATH/sie.war
 
     # Restore Configuration
-    sudo cp $DEMO_ENV/logback.xml $DEPLOY_TARGET_PATH/sie/WEB-INF/classes/
+    #sudo cp $DEMO_ENV/logback.xml $DEPLOY_TARGET_PATH/sie/WEB-INF/classes/
     sudo rm -f $DEPLOY_TARGET_PATH/sie/WEB-INF/classes/config/application-env.yml
     sudo cp $DEMO_ENV/data-location.properties $DEPLOY_TARGET_PATH/sie/WEB-INF/classes/config/
     
-    sudo chown -R metamac.metamac /servers/metamac
-    sudo service sie01 start
-    
+    if [ $RESTART -eq 1 ]; then
+        sudo chown -R metamac.metamac /servers/metamac
+        sudo service metamac01 start
+    fi
 
     echo "Finished deploy"
 
