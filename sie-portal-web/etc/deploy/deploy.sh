e777c89e6b713a3a983cab6e84095c87258215a6
Javier Villamonte <jvilper@arte-consultores.com>
2018-05-22 09:30:33 +0100

    METAMAC-2750 - Despliegue en la infraestructura de DEMOS
    >Se modifica el script de despliegue.
    >Se borran las cosas relacionadas con el CAS. No se van a usar.

--- sie-portal-web/etc/deploy/deploy.sh
+++ sie-portal-web/etc/deploy/deploy.sh
@@ -1,37 +1,35 @@
 #!/bin/sh
 
-# FIXME: Eliminar referencias a la plantilla (es.gobcan.istac.sie, sie, plantilla, etc...)
-
-HOME_PATH=plantilla
+HOME_PATH=sie
 TRANSFER_PATH=$HOME_PATH/tmp
-DEPLOY_TARGET_PATH=/servers/plantilla/tomcats/plantilla01/webapps
+DEPLOY_TARGET_PATH=/servers/metamac/tomcats/sie01/webapps
 DEMO_ENV=$HOME_PATH/env
 
-scp -r etc/deploy deploy@tacoronte.arte:$TRANSFER_PATH
-scp target/sie-*.war deploy@tacoronte.arte:$TRANSFER_PATH/plantilla.war
-ssh deploy@tacoronte.arte <<EOF
+scp -r etc/deploy deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH
+scp target/sie-portal-web-*.war deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH/sie.war
+ssh deploy@estadisticas.arte-consultores.com <<EOF
 
     chmod a+x $TRANSFER_PATH/deploy/*.sh;
     
-    sudo service plantilla01 stop
+    sudo service sie01 stop
     
     ###
     # ARTE-APPLICATION-TEMPLATE
     ###
 
     # Update Process
-    sudo rm -rf $DEPLOY_TARGET_PATH/plantilla
-    sudo mv $TRANSFER_PATH/plantilla.war $DEPLOY_TARGET_PATH/plantilla.war
-    sudo unzip $DEPLOY_TARGET_PATH/plantilla.war -d $DEPLOY_TARGET_PATH/plantilla
-    sudo rm -rf $DEPLOY_TARGET_PATH/plantilla.war
+    sudo rm -rf $DEPLOY_TARGET_PATH/sie
+    sudo mv $TRANSFER_PATH/sie.war $DEPLOY_TARGET_PATH/sie.war
+    sudo unzip $DEPLOY_TARGET_PATH/sie.war -d $DEPLOY_TARGET_PATH/sie
+    sudo rm -rf $DEPLOY_TARGET_PATH/sie.war
 
     # Restore Configuration
-    sudo cp $DEMO_ENV/logback.xml $DEPLOY_TARGET_PATH/plantilla/WEB-INF/classes/
-    sudo rm -f $DEPLOY_TARGET_PATH/plantilla/WEB-INF/classes/config/application-env.yml
-    sudo cp $DEMO_ENV/data-location.properties $DEPLOY_TARGET_PATH/plantilla/WEB-INF/classes/config/
+    sudo cp $DEMO_ENV/logback.xml $DEPLOY_TARGET_PATH/sie/WEB-INF/classes/
+    sudo rm -f $DEPLOY_TARGET_PATH/sie/WEB-INF/classes/config/application-env.yml
+    sudo cp $DEMO_ENV/data-location.properties $DEPLOY_TARGET_PATH/sie/WEB-INF/classes/config/
     
-    sudo chown -R plantilla.plantilla /servers/plantilla
-    sudo service plantilla01 start
+    sudo chown -R metamac.metamac /servers/metamac
+    sudo service sie01 start
     
 
     echo "Finished deploy"
