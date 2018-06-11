5b080a5cb175d3c8a3c960f0fda06bbbfb5c0a4e
Javier Villamonte <jvilper@arte-consultores.com>
2018-06-11 12:13:56 +0100

    METAMAC-2739 - Crear página 1 de listado de lugares sobre los que obtener el histórico electoral
    >Se crea un componente nuevo para el footer.
    >Se modifican los estilos.
    >Se adapta la interfaz al nuevo diseño.

--- sie-portal-web/src/main/webapp/app/layouts/index.ts
+++ sie-portal-web/src/main/webapp/app/layouts/index.ts
@@ -3,3 +3,4 @@ export * from './error/error.route';
 export * from './main/main.component';
 export * from './layout-routing.module';
 export * from './navbar/navbar.component';
+export * from './footer/footer.component';
