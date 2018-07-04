beef05a8d85a67865d4eff20e35dfc2e32b5c35d
Javier Villamonte <jvilper@arte-consultores.com>
2018-07-04 15:17:58 +0100

    METAMAC-2752 - Crear página 2 de listado de procesos electorales
    >El título de la aplicación debe llevar a la página 1.

--- sie-portal-web/src/main/webapp/app/interfaces/interfaces.module.ts
+++ sie-portal-web/src/main/webapp/app/interfaces/interfaces.module.ts
@@ -1,16 +1,27 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { TitleBarComponent } from './title-bar/title-bar.component';
+import { SieSharedModule } from '../shared';
+import { RouterModule } from '@angular/router';
+import { lugarRoute, LugarComponent } from './lugar';
+import { SieDocumentoModule } from '../documento/documento.module';
+import { evolucionElectoralRoute, EvolucionElectoralComponent } from './evolucion-electoral';
 
-import { SieLugarModule } from './lugar/lugar.module';
-import { SieEvolucionElectoralModule } from './evolucion-electoral/evolucion-electoral.module';
+const ENTITY_STATES = [
+    ...lugarRoute,
+    ...evolucionElectoralRoute
+];
 
 @NgModule({
     imports: [
-        SieLugarModule,
-        SieEvolucionElectoralModule
+        SieSharedModule,
+        SieDocumentoModule,
+        RouterModule.forRoot(ENTITY_STATES, { useHash: true })
     ],
-    declarations: [],
-    entryComponents: [],
-    providers: [],
+    declarations: [
+        TitleBarComponent,
+        LugarComponent,
+        EvolucionElectoralComponent
+    ],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class SieInterfacesModule {}
