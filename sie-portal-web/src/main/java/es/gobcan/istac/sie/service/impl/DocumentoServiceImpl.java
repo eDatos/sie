697b71b5e41b6f1d98e7214f1ae91052dbcbef2c
Javier Villamonte <jvilper@arte-consultores.com>
2018-07-24 10:10:14 +0100

    METAMAC-2755 - Genarar informe PDF de la página 2
    >Se corrige el bug. No se encontraba el subinforme.

--- sie-portal-web/src/main/java/es/gobcan/istac/sie/service/impl/DocumentoServiceImpl.java
+++ sie-portal-web/src/main/java/es/gobcan/istac/sie/service/impl/DocumentoServiceImpl.java
@@ -19,7 +19,7 @@ public class DocumentoServiceImpl implements DocumentoService {
 
     private static final String LOGO_CABECERA = "logo_istac.png";
     private static final String EVOLUCION_ELECTORAL_TEMPLATE = "evolucion-electoral.jasper";
-    private static final String EVOLUCION_ELECTORAL_SUBINFORME_TEMPLATE = "tablas.jasper";
+    private static final String RUTA_RELATIVA_DIRECTORIO_SUBINFORME = "./jasper/";
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DocumentoServiceImpl.class);
 
@@ -37,8 +37,7 @@ public class DocumentoServiceImpl implements DocumentoService {
         parametros.put("TERRITORIO", evolucionElectoral.getTerritorio());
         parametros.put("TIPO_ELECCIONES", evolucionElectoral.getTipoElecciones());
         parametros.put("dataSource", new JRBeanCollectionDataSource(evolucionElectoral.getProcesosElectorales()));
-        String rutaSubinforme = getClass().getResource(Constants.CARPETA_JASPER_REPORT + EVOLUCION_ELECTORAL_SUBINFORME_TEMPLATE).getPath();
-        parametros.put("rutaSubinforme", rutaSubinforme);
+        parametros.put("SUBREPORT_DIR", RUTA_RELATIVA_DIRECTORIO_SUBINFORME);
         String logoPath = getClass().getResource(Constants.CARPETA_JASPER_REPORT + LOGO_CABECERA).getPath();
         parametros.put("rutaLogo", logoPath);
         return this.reportsService.generateFromTemplate(EVOLUCION_ELECTORAL_TEMPLATE, parametros, null);
