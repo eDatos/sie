92cfba7bd3f9755c78246b1be0be638ce1f8516f
Javier Villamonte <jvilper@arte-consultores.com>
2018-07-30 08:54:11 +0100

    METAMAC-2794 - Crear página 3 de gráficos
    >Se cargan solo las partes de bootstrap que se necesitan.
    >Se realiza la carga solo en el visualizador.

--- sie-portal-web/src/main/webapp/app/layouts/navbar/navbar.component.ts
+++ sie-portal-web/src/main/webapp/app/layouts/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { TemplateService } from '../template';
 
 @Component({
@@ -10,10 +10,28 @@ export class NavbarComponent implements OnInit {
     public navbar: string;
 
     constructor(
+        private elementRef: ElementRef,
         private templateService: TemplateService
     ) { }
 
     ngOnInit() {
-        this.templateService.getNavbar().subscribe((navbarHtml) => this.navbar = navbarHtml);
+        this.templateService.getNavbar().subscribe((navbarHtml) => {
+            this.navbar = navbarHtml;
+            setTimeout(() => this.reinsertScripts());
+        });
+    }
+
+    private reinsertScripts() {
+        const scriptList = this.elementRef.nativeElement.getElementsByTagName('script');
+        for (const script of scriptList) {
+            const scriptCopy = document.createElement('script');
+            if (script.innerHTML) {
+                scriptCopy.innerHTML = script.innerHTML;
+            } else if (script.src) {
+                scriptCopy.src = script.src;
+            }
+            scriptCopy.async = false;
+            script.parentNode.replaceChild(scriptCopy, script);
+        }
     }
 }
