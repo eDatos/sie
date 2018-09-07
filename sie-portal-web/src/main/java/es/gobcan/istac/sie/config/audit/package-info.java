7ef2886fcb2c53ec822ce31b8406d3a96487aec1
Javier Villamonte <jvilper@arte-consultores.com>
2018-09-07 13:49:51 +0100

    METAMAC-2738 - Limpiar el proyecto plantilla para utilizarlo como proyecto de frontoffice
    >Se quitan clases y ficheros innecesarios del backend.

--- sie-portal-web/src/main/java/es/gobcan/istac/sie/config/audit/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Audit specific code.
- */
-package es.gobcan.istac.sie.config.audit;
--- sie-portal-web/src/main/java/es/gobcan/istac/sie/optimistic/RootLink.java
+++
@@ -1,7 +0,0 @@
-package es.gobcan.istac.sie.optimistic;
-
-@FunctionalInterface
-public interface RootLink<T extends VersionedEntity> {
-
-    T root();
-}
