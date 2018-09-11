d1e1ec1a44eeaa5e48e32ef68330b577035dd01e
Javier Villamonte <jvilper@arte-consultores.com>
2018-09-11 08:16:04 +0100

    METAMAC-2765 - Externalizar configuración de propiedades
    >Al cambiar la manera de obtener la configuración ha cambiado la manera de ejecutar el código de google analytics.

--- sie-portal-web/src/main/webapp/app/layouts/main/main.component.ts
+++ sie-portal-web/src/main/webapp/app/layouts/main/main.component.ts
@@ -3,6 +3,9 @@ import { Router, ActivatedRouteSnapshot, NavigationEnd, RoutesRecognized } from 
 
 import { JhiLanguageService } from 'ng-jhipster';
 import { JhiLanguageHelper, StateStorageService } from '../../shared';
+import { MetadataService, ConfigService } from '../../config';
+
+declare var executeGoogleAnalyticsInlineCode: Function;
 
 @Component({
     selector: 'jhi-main',
@@ -11,13 +14,40 @@ import { JhiLanguageHelper, StateStorageService } from '../../shared';
 })
 export class JhiMainComponent implements OnInit {
 
+    private googleAnalyticsTrackId: string;
+
     constructor(
         private jhiLanguageHelper: JhiLanguageHelper,
-        private languageService: JhiLanguageService,
         private router: Router,
+        private configService: ConfigService,
+        private metadataService: MetadataService,
+        private languageService: JhiLanguageService,
         private $storageService: StateStorageService
-    ) {
+    ) { }
 
+    ngOnInit() {
+        const config = this.configService.getConfig();
+        this.metadataService.getPropertyById(config.metadata.googleTrackingIdKey).subscribe((googleAnalyticsTrackId) => {
+            this.googleAnalyticsTrackId = googleAnalyticsTrackId;
+            this.addGoogleAnalyticsScriptTag(googleAnalyticsTrackId);
+            executeGoogleAnalyticsInlineCode(googleAnalyticsTrackId);
+        });
+
+        this.router.events.subscribe((event) => {
+            if (event instanceof NavigationEnd) {
+                this.jhiLanguageHelper.updateTitle(this.getPageTitle(this.router.routerState.snapshot.root));
+                if (this.googleAnalyticsTrackId) {
+                    executeGoogleAnalyticsInlineCode(this.googleAnalyticsTrackId);
+                }
+            }
+        });
+    }
+
+    private addGoogleAnalyticsScriptTag(googleAnalyticsTrackId: string) {
+        const googleAnalyticsScript = document.createElement('script');
+        googleAnalyticsScript.setAttribute('src', 'https://www.googletagmanager.com/gtag/js?id=' + googleAnalyticsTrackId);
+        googleAnalyticsScript.async = true;
+        document.head.appendChild(googleAnalyticsScript);
     }
 
     private getPageTitle(routeSnapshot: ActivatedRouteSnapshot) {
@@ -26,13 +56,5 @@ export class JhiMainComponent implements OnInit {
             title = this.getPageTitle(routeSnapshot.firstChild) || title;
         }
         return title;
-    }
-
-    ngOnInit() {
-        this.router.events.subscribe((event) => {
-            if (event instanceof NavigationEnd) {
-                this.jhiLanguageHelper.updateTitle(this.getPageTitle(this.router.routerState.snapshot.root));
-            }
-        });
     }
 }
