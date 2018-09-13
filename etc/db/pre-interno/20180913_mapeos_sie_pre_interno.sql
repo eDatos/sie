86f18076e4c3658c6a0ed039776a67c82a8185b7
Javier Villamonte <jvilper@arte-consultores.com>
2018-09-13 14:12:34 +0100

    NONE - Se dan de alta los mapeos entre tipos de elecciones y multidatasets en el entorno de PRE INTERNO

---
+++ etc/db/pre-interno/20180913_mapeos_sie_pre_interno.sql
@@ -0,0 +1,14 @@
+-- Script de inserciÃ³n de un mapeo entre tipo de elecciones y su multidataset.
+
+-- Ejemplo:
+-- insert into tb_tipo_elecciones_dataset_url (tipo_elecciones, dataset_url) values ('MUNICIPALES', '/multidatasets/ISTAC/C00010A_000003');
+
+insert into tb_tipo_elecciones_dataset_url (tipo_elecciones, dataset_url) values ('MUNICIPALES', '/multidatasets/ISTAC/C00010A_000006');
+insert into tb_tipo_elecciones_dataset_url (tipo_elecciones, dataset_url) values ('CABILDO', '/multidatasets/ISTAC/C00010A_000007');
+insert into tb_tipo_elecciones_dataset_url (tipo_elecciones, dataset_url) values ('AUTONOMICAS', '/multidatasets/ISTAC/C00010A_000002');
+insert into tb_tipo_elecciones_dataset_url (tipo_elecciones, dataset_url) values ('CONGRESO', '/multidatasets/ISTAC/C00010A_000001');
+insert into tb_tipo_elecciones_dataset_url (tipo_elecciones, dataset_url) values ('SENADO', '/multidatasets/ISTAC/C00010A_000003');
+insert into tb_tipo_elecciones_dataset_url (tipo_elecciones, dataset_url) values ('PARLAMENTO_EUROPEO', '/multidatasets/ISTAC/C00010A_000005');
+insert into tb_tipo_elecciones_dataset_url (tipo_elecciones, dataset_url) values ('REFERENDUM', '/multidatasets/ISTAC/C00010A_000004');
+
+commit;
