29889399f83dc0e13ba6151e38384becec3d35d8
amonfue <amonfue@arte-consultores.com>
2019-08-01 09:03:08 +0100

    METAMAC-2947 - No se cargan los mapas al cambiar el nivel en la... ...dimensión de "Territorio"
    > Se establece un rango correcto cuando no hay valores a representar.
    
    > Se obtienen las representaciones geográficas según el nivel
    seleccionado.

--- sie-graphics/src/main/js/metamac/modules/dataset/visual-element/Map.js
+++ sie-graphics/src/main/js/metamac/modules/dataset/visual-element/Map.js
@@ -147,8 +147,8 @@
                 return value.value;
             });
             this._mapModel.set("values", values);
-            this._mapModel.set("minValue", _.min(values));
-            this._mapModel.set("maxValue", _.max(values));
+            this._mapModel.set("minValue", values.length > 0? _.min(values): 0);
+            this._mapModel.set("maxValue", values.length > 0? _.max(values): 0);
         },
 
         _initContainerView: function () {
@@ -239,7 +239,7 @@
         },
 
         _getGeographicSelectedRepresentations: function () {
-            return this._getGeographicDimension().get('representations').where({ drawable: true });
+            return this._getGeographicDimension().get('representations').getSelectedRepresentationsByCurrentLevel();
         },
 
         _getGeographicDimensionNormCodes: function () {
